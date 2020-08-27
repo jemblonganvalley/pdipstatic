63569c005a45dc74ab4ccb273cd40d24dd3cc3aa
mohamad-ridwan <64695651+mohamad-ridwan@users.noreply.github.com>
2020-08-27 19:50:19 +0700

    update page card thumbnail

---
+++ src/section/bkbb/thumbnailwejangan/DataThumbnailWejangan.js
@@ -0,0 +1,28 @@
+// Database Card Wejangan BKBB
+let DataWejanganBk = [
+    {
+        pageWejanganBk: '',
+        imgWejanganBk: '',
+        icon1WejanganBk: 'fas fa-quote-right',
+        txt1WejanganBk: 'NASIONALISME KITA ADALAH NASIONALISME YANG MEMBUAT KITA MENJADI "PERKAKASNYA TUHAN", DAN MEMBUAT KITA MENJADI "HIDUP DI DALAM ROH".',
+        icon2WejanganBk: 'fas fa-quote-right',
+        txt2WejanganBk: 'Soekarno, Suluh Indonesia Muda, 1928'
+    },
+    {
+        pageWejanganBk: '',
+        imgWejanganBk: '',
+        icon1WejanganBk: 'fas fa-quote-right',
+        txt1WejanganBk: 'ORANG TIDAK DAPAT MENGABDI KEPADA TUHAN DENGAN TIDAK MENGABDI KEPADA SESAMA MANUSIA. TUHAN BERSEMAYAM DI GUBUKNYA SI MISKIN',
+        icon2WejanganBk: 'fas fa-quote-right',
+        txt2WejanganBk: 'Soekarno, 23 Oktober 1946'
+    },
+    {
+        pageWejanganBk: '',
+        imgWejanganBk: '',
+        icon1WejanganBk: 'fas fa-quote-right',
+        txt1WejanganBk: 'TUJUAN SAYA IALAH UNTUK MENJADI PEMBANGUN DARI SUATU BANGSA',
+        icon2WejanganBk: 'fas fa-quote-right',
+        txt2WejanganBk: 'Soekarno, Suluh Indonesia Muda, 1922'
+    },
+]
+// END Database Card Wejangan BKBB
